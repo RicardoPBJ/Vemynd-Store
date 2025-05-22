14525ec90294e47ff388a0f3e6afbd1828f2e3d7
Ricardo Pereira <rbrumjunio@gmail.com>
2025-05-21 22:53:40 -0300

    configurando fluentassertions e adicionando Usings para os testes

--- backend/VemyndStore.Api.Tests/Usings.cs
+++ backend/VemyndStore.Api.Tests/Usings.cs
@@ -3,3 +3,5 @@ global using Moq;
 global using FluentAssertions;
 global using VemyndStore.Api.Data.Models;
 global using VemyndStore.Api.Validators;
+global using VemyndStore.Api.Exceptions;
+global using VemyndStore.Api.Middleware;
